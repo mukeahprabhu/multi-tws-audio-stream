7753fe47b4bfec7f053bbfbef4152b36bda8f8b1
mukeshp <164596178+mukeahprabhu@users.noreply.github.com>
2025-04-25 06:22:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const WebSocket = require("ws");
 const PORT = process.env.PORT || 10000;
 const wss = new WebSocket.Server({ port: PORT });
 
-console.log(WebSocket server running on port ${PORT});
+console.log(`WebSocket server running on port ${PORT}`);
 
 wss.on("connection", (ws) => {
   console.log("🔌 New client connected");
